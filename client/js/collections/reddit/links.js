1998e1deee06e55b812702201abe665c2809fc86
micha <micha@famreiser.ch>
2014-11-05 14:06:39 +0100

    new Port

--- client/js/collections/reddit/links.js
+++ client/js/collections/reddit/links.js
@@ -3,6 +3,6 @@ define(['collection', "models/reddit/link"], function (Collection, Link) {
     name: 'reddit/links',
     model: Link,
 
-    url: "http://localhost:4730/entries"
+    url: "http://localhost:8888/entries"
   });
 });
